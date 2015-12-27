ba3bbe64009b1451457cdac042b0ce2ea0b9a3d2
Shadeoflance <shadeoflance@gmail.com>
2015-12-27 20:00:09 +0300

    Basic player and obstacle implementations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float Speed = 3;
+	Rigidbody2D rb;
+	void Awake()
+	{
+		rb = (Rigidbody2D)GetComponent (typeof(Rigidbody2D));
+	}
+
+	void Update () 
+	{
+		rb.velocity = Vector2.zero;
+		rb.velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis ("Vertical"));
+		rb.velocity.Normalize ();
+		rb.velocity *= Speed;
+	}
+}
