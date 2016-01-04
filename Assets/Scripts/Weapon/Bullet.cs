b8603c05af2c380601aeddfe89db4b6f398bb98d
Shadeoflance <shadeoflance@gmail.com>
2016-01-04 13:11:31 +0300

    Bullet collosion masks

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour 
 {
+	public int KillMask;
+	public int NoCollisionMask;
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		GameObject obj = col.gameObject;
-		if((obj.layer & LayerMask.NameToLayer("Enemy")) != 0)
-			Destroy(obj);
-		if(obj.layer == LayerMask.NameToLayer("Player"))
+		int layer = 1 << obj.layer;
+		if((layer & NoCollisionMask) != 0)
 			return;
+		if((layer & KillMask) != 0)
+			Destroy(obj);
 		Destroy(gameObject);
 	}
 }
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -3,5 +3,7 @@ using System.Collections;
 
 public abstract class Weapon : MonoBehaviour 
 {
+	public LayerMask KillMask;
+	public LayerMask NoCollisionMask;
 	public abstract void Shoot(Vector2 dir);
 }
